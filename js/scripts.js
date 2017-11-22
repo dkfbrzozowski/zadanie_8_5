542e72b75225e75024166429c9d4710b8f690bf5
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-11-22 17:46:37 +0100

    Count triangle area

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = prompt ('Proszę podać wartość zmiennej "a"'),
+    h = prompt ('Proszę podać wartośc zmiennej "h"'),
+    triangleArea = a * h / 2;
+
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
